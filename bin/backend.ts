b1cc783645e49788e6bf63e81e2b4c9a849093fe
Cory Martin <murribu@gmail.com>
2024-12-16 15:43:36 -0600

    refactor: Refactor backend deployment structure
    Improved modularity and maintainability of the CDK application's backend. Replaced the original `BackendStack` with smaller, focused stacks (`Appsync`, `AppsyncPre`, `AppsyncResolvers`, `Codebuild`, `Cognito`, `DynamoDb`, `Layers`).  Dependencies are explicitly defined using `addDependency`.  Added a `config` file and set `env` to `us-east-1`. Included `source-map-support/register` for improved debugging.

--- bin/backend.ts
+++ bin/backend.ts
@@ -1,20 +1,45 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { BackendStack } from '../lib/backend-stack';
+#!/usr/bin/env nodeimport "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { Appsync } from "../lib/appsync";
+import { AppsyncPre } from "../lib/appsync-pre";
+import { AppsyncResolvers } from "../lib/appsync-resolvers";
+import { Codebuild } from "../lib/codebuild";
+import { Cognito } from "../lib/cognito";
+import { DynamoDb } from "../lib/dynamodb";
+import { Layers } from "../lib/layers";
 
+import config from "../config";
+
+const { PROJECT_NAME } = config;
+
+const env = { region: "us-east-1" };
 const app = new cdk.App();
-new BackendStack(app, 'BackendStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const layers = new Layers(app, `${PROJECT_NAME}Layers`, { env });
+const dynamodb = new DynamoDb(app, `${PROJECT_NAME}DynamoDb`, {
+  env,
+});
+dynamodb.addDependency(layers);
+const appsyncPre = new AppsyncPre(app, `${PROJECT_NAME}AppsyncPre`, {
+  dynamodb,
+  stackProps: { env },
+});
+const cognito = new Cognito(app, `${PROJECT_NAME}Cognito`, {
+  dynamodb,
+  stackProps: { env },
+});
+cognito.addDependency(layers);
+const appsync = new Appsync(app, `${PROJECT_NAME}Appsync`, {
+  stackProps: { env },
+  cognito,
+  dynamodb,
+  appsyncPre,
+});
+appsync.addDependency(layers);
+new AppsyncResolvers(app, `${PROJECT_NAME}AppsyncResolvers`, {
+  stackProps: { env },
+  appsync,
+});
+new Codebuild(app, `${PROJECT_NAME}BackendCodebuild`, {
+  env,
 });
