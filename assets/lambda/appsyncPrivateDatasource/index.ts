e345030d034b519000acd6e491e4705a93c5cd73
Cory Martin <murribu@gmail.com>
2024-12-17 11:15:54 -0600

    feat: Implement DynamoDB data access layer for books and tags
    This commit introduces a new DynamoDB data access layer for books and tags in the AppSync application.  Changes include:
    
    - Added `books.ts`, `tags.ts`, `index.ts` to `assets/lambda/appsyncPrivateDatasource` for CRUD operations.
    - Moved CRUD operations from public to private datasource.
    - Updated `books.ts` and `index.ts` in `assets/lambda/appsyncPublicDatasource` to only include list operations.
    - Removed `tags.ts` from `assets/lambda/appsyncPublicDatasource`.

--- assets/lambda/appsyncPrivateDatasource/index.ts
+++ assets/lambda/appsyncPrivateDatasource/index.ts
@@ -1,16 +1,38 @@
 import { DynamoDB, QueryCommandInput } from "@aws-sdk/client-dynamodb";
-import { listBooks } from "./books";
-import { listTags } from "./tags";
+import {
+  CreateBookArgs,
+  DeleteBookArgs,
+  createBook,
+  deleteBook,
+  listBooks,
+  updateBook,
+} from "./books";
+import {
+  CreateTagsArgs,
+  createTags,
+  deleteTags,
+  listTags,
+  tagBook,
+  untagBook,
+} from "./tags";
 
 enum AppsyncFunction {
+  CreateBook = "createBook",
+  UpdateBook = "updateBook",
+  DeleteBook = "deleteBook",
   ListBooks = "listBooks",
+  TagBook = "tagBook",
+  UntagBook = "untagBook",
   ListTags = "listTags",
+  CreateTags = "createTags",
+  DeleteTag = "deleteTag",
   ListLEAs = "listLEAs",
 }
 
 export interface AppsyncRequestBase {
   function: AppsyncFunction;
   sub: string;
+  args: CreateBookArgs | DeleteBookArgs | CreateTagsArgs;
 }
 
 export const ddb = new DynamoDB({ apiVersion: "2006-03-01" });
@@ -18,10 +40,24 @@ export const ddb = new DynamoDB({ apiVersion: "2006-03-01" });
 export const handler = async (event: AppsyncRequestBase) => {
   console.log("event", event);
   switch (event.function) {
+    case AppsyncFunction.CreateBook:
+      return createBook(event);
+    case AppsyncFunction.UpdateBook:
+      return updateBook(event);
+    case AppsyncFunction.DeleteBook:
+      return deleteBook(event);
     case AppsyncFunction.ListBooks:
       return listBooks();
+    case AppsyncFunction.TagBook:
+      return tagBook(event);
+    case AppsyncFunction.UntagBook:
+      return untagBook(event);
     case AppsyncFunction.ListTags:
       return listTags();
+    case AppsyncFunction.CreateTags:
+      return createTags(event);
+    case AppsyncFunction.DeleteTag:
+      return deleteTags(event);
     case AppsyncFunction.ListLEAs:
       return listLEAs();
     default:
---
+++ assets/lambda/appsyncPrivateDatasource/tags.ts
@@ -0,0 +1,149 @@
+import {
+  BatchWriteItemCommandInput,
+  PutItemCommandInput,
+  QueryCommandInput,
+  UpdateItemCommandInput,
+} from "@aws-sdk/client-dynamodb";
+import { AppsyncRequestBase, ddb } from ".";
+
+export interface CreateTagsArgs {
+  tags: string[];
+}
+
+export interface TagBookArgs {
+  isbn: string;
+  tags: string[];
+}
+
+export const createTags = async (event: AppsyncRequestBase) => {
+  const a = event.args as CreateTagsArgs;
+  //get the omni item and add the tags
+  const queryParams: QueryCommandInput = {
+    TableName: process.env.DYNAMO_TABLE!,
+    KeyConditionExpression: "PK = :pk AND SK = :sk",
+    ExpressionAttributeValues: {
+      ":pk": { S: `omni` },
+      ":sk": { S: "i" },
+    },
+  };
+  const result = await ddb.query(queryParams);
+
+  if (!result.Items || result.Items?.length === 0) {
+    const putItemCommandInput: PutItemCommandInput = {
+      TableName: process.env.DYNAMO_TABLE!,
+      Item: {
+        PK: { S: `omni` },
+        SK: { S: "i" },
+        tags: { SS: a.tags },
+      },
+    };
+    const putItemResult = await ddb.putItem(putItemCommandInput);
+    console.log({ putItemResult: JSON.stringify(putItemResult) });
+  } else {
+    const tags = result.Items[0].tags?.SS || [];
+    const updateItemCommandInput: UpdateItemCommandInput = {
+      TableName: process.env.DYNAMO_TABLE!,
+      Key: {
+        PK: { S: `omni` },
+        SK: { S: "i" },
+      },
+      UpdateExpression: "SET tags = list_append(tags, :tags)",
+      ExpressionAttributeValues: {
+        ":tags": { SS: a.tags },
+      },
+    };
+    const updateItemResult = await ddb.updateItem(updateItemCommandInput);
+    console.log({ updateItemResult: JSON.stringify(updateItemResult) });
+  }
+};
+
+export const tagBook = async (event: AppsyncRequestBase) => {
+  const a = event.args as TagBookArgs;
+
+  const batchWriteItemCommandInput: BatchWriteItemCommandInput = {
+    RequestItems: {
+      [process.env.DYNAMO_TABLE!]: a.tags.map((tag) => ({
+        PutRequest: {
+          Item: {
+            PK: { S: `book#${a.isbn}` },
+            SK: { S: `tag#${tag}` },
+            createdBy: { S: event.sub },
+            createdAt: { S: new Date().toISOString() },
+          },
+        },
+      })),
+    },
+  };
+  const result = await ddb.batchWriteItem(batchWriteItemCommandInput);
+  console.log({ result: JSON.stringify(result) });
+};
+
+export const untagBook = async (event: AppsyncRequestBase) => {
+  const a = event.args as TagBookArgs;
+
+  const batchWriteItemCommandInput: BatchWriteItemCommandInput = {
+    RequestItems: {
+      [process.env.DYNAMO_TABLE!]: a.tags.map((tag) => ({
+        DeleteRequest: {
+          Key: {
+            PK: { S: `book#${a.isbn}` },
+            SK: { S: `tag#${tag}` },
+          },
+        },
+      })),
+    },
+  };
+  const result = await ddb.batchWriteItem(batchWriteItemCommandInput);
+  console.log({ result: JSON.stringify(result) });
+};
+
+export const listTags = async () => {
+  const queryParams: QueryCommandInput = {
+    TableName: process.env.DYNAMO_TABLE!,
+    KeyConditionExpression: "PK = :pk and SK = :sk",
+    ExpressionAttributeValues: {
+      ":pk": { S: "omni" },
+      ":sk": { S: "i" },
+    },
+    AttributesToGet: ["PK", "SK", "tags"],
+  };
+  const result = await ddb.query(queryParams);
+  if (result.Items && result.Items.length > 0) {
+    return result.Items[0].tags?.L || [];
+  } else {
+    return [];
+  }
+};
+
+export const deleteTags = async (event: AppsyncRequestBase) => {
+  const a = event.args as CreateTagsArgs;
+
+  const queryParams: QueryCommandInput = {
+    TableName: process.env.DYNAMO_TABLE!,
+    KeyConditionExpression: "PK = :pk AND SK = :sk",
+    ExpressionAttributeValues: {
+      ":pk": { S: `omni` },
+      ":sk": { S: "i" },
+    },
+    AttributesToGet: ["PK", "SK", "tags"],
+  };
+  const result = await ddb.query(queryParams);
+  if (!result.Items || result.Items?.length === 0) {
+    return;
+  }
+  const tags = result.Items[0].tags?.SS || [];
+  const newTags = tags.filter((tag) => !a.tags.includes(tag));
+  const updateItemCommandInput: UpdateItemCommandInput = {
+    TableName: process.env.DYNAMO_TABLE!,
+    Key: {
+      PK: { S: `omni` },
+      SK: { S: "i" },
+    },
+    UpdateExpression: "SET tags = :tags",
+    ExpressionAttributeValues: {
+      ":tags": { SS: newTags },
+    },
+  };
+  const updateItemResult = await ddb.updateItem(updateItemCommandInput);
+  console.log({ updateItemResult: JSON.stringify(updateItemResult) });
+};
